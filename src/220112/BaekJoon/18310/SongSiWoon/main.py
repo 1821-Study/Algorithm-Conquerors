0709d6d85f6bb4e5a2bfc4900b20041caacae81b
Dltmd202 <dltmd202@gmail.com>
2022-01-15 21:29:23 +0900

    Rector: Reform directory structure

--- src/220112/BaekJoon/18310/SongSiWoon/main.py
+++
@@ -1,6 +0,0 @@
-N = int(input())
-home = list(map(int, input().split()))
-
-home.sort()
-res = home[(N-1)//2]
-print(res)
--- src/220115/2444/ahnjongin/main.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-for i in range(1, n):
-    print(' '*(n-i) + '*'*(2*i-1))
-for i in range(n, 0, -1):
-    print(' '*(n-i) + '*'*(2*i-1))
