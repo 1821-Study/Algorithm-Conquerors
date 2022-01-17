232ba6869fa4b37df0f88b1c8bd150651f6b8a2d
songdakyung <sdk4590@koreatech.ac.kr>
2022-01-18 01:02:57 +0900

    Feat: Add solution of #26

--- src/BaekJoon/sa11k/5585/Main.java
+++ src/BaekJoon/sa11k/5585/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Scanner sc = new Scanner(System.in);
         int money = sc.nextInt();
 
