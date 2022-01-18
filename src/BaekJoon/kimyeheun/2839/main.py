961a1d59fa3de92fbbff4c272ecd1686bb4ffb8e
김예은 <yeheun004@gmail.com>
2022-01-18 23:37:46 +0900

    Feat:Add solution of #26

--- src/BaekJoon/kimyeheun/2839/main.py
+++ src/BaekJoon/kimyeheun/2839/main.py
@@ -1,12 +1,16 @@
 n = int(input())
+check = []
 count = 0
-
-while n >= 0:
-    if n%3 == 0:
-        count += n//3
-        print(count)
-        break
-    n -= 5
-    count += 1
-else:
+for i in range(0, 2000):
+    for u in range(0, 1010):
+        if n-(3*i + 5*u) == 0:
+            check.append(i+u)
+            count += 1
+if count == 0:
     print(-1)
+else:
+    minni = check[0]
+    for y in range(1, len(check)):
+        if minni > check[y]:
+            minni = check[y]
+print(minni)
