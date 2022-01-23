fc3aa99963c77af70174e5bb05d70b144eb2542d
SongSiWoon <siwoons7319@gmail.com>
2022-01-23 14:21:23 +0900

    Fix: Modify solution of #51

--- src/BaekJoon/SongSiWoon/2493/Main.java
+++ src/BaekJoon/SongSiWoon/2493/Main.java
@@ -7,8 +7,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     private static int N;
-    private static ArrayList<Integer> towers;
-    private static Stack<Integer> res;
+    private static ArrayList<int[]> towers;
+    private static Stack<int[]> res;
     private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -18,7 +18,8 @@ public class Main {
         towers = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            towers.add(Integer.valueOf(st.nextToken()));
+            int[] tmp = {Integer.parseInt(st.nextToken()), i};
+            towers.add(tmp);
         }
 
         res = new Stack<>();
@@ -27,7 +28,7 @@ public class Main {
     }
 
     public static void sol() {
-        for (int tower : towers) {
+        for (int[] tower : towers) {
             if (res.isEmpty()) {
                 res.push(tower);
                 sb.append("0").append(" ");
@@ -38,12 +39,12 @@ public class Main {
                         res.push(tower);
                         break;
                     }
-                    int before = res.peek();
-                    if (before < tower) {
+                    int[] before = res.peek();
+                    if (before[0] < tower[0]) {
                         res.pop();
                     } else {
                         res.push(tower);
-                        sb.append(towers.indexOf(before) + 1).append(" ");
+                        sb.append(before[1] + 1).append(" ");
                         break;
                     }
                 }
