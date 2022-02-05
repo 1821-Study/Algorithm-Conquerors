d648b7a0babd28d5eb2e5a1e442d3fabf6d3bc24
SongSiWoon <siwoons7319@gmail.com>
2022-02-05 14:43:22 +0900

    Fix: Modify solution of #75

--- src/BaekJoon/SongSiWoon/1300/Main.java
+++ src/BaekJoon/SongSiWoon/1300/Main.java
@@ -11,20 +11,20 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         K = Integer.parseInt(br.readLine());
-        System.out.println(sol(1, (int) Math.pow(N,2)));
+        System.out.println(sol(1L, (long) Math.pow(N,2)));
     }
 
-    public static int sol(int start, int end) {
+    public static long sol(long start, long end) {
         while (start < end) {
-            int mid = (start + end) / 2;
-            int cnt = 0;
+            long mid = (start + end) / 2;
+            long cnt = 0;
 
             for (int i = 1; i <= N; i++) {
-                int tmp = Math.min(mid / i, N);
+                long tmp = Math.min(mid / i, N);
                 if (tmp == 0) break;
                 cnt += tmp;
             }
-            if (cnt > K) {
+            if (cnt >= K) {
                 end = mid;
             } else {
                 start = mid + 1;
