86874e181e93af5ace8668658c58361d35976a37
Dltmd202 <dltmd202@gmail.com>
2022-07-20 11:59:18 +0900

    Feat: Add solution of #118

---
+++ src/BaekJoon/Dltmd202/2798/Main.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] data = new int[n];
+        boolean[] visited = new boolean[n];
+        int answer = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            data[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int ret = data[i] + data[j] + data[k];
+                    if(ret > answer && ret <= m){
+                        answer = ret;
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
